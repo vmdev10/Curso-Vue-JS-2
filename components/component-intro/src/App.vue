c4c9cde7c474ed532e3b9b4db20d8dac1e14afd8
Vitória Moura <mouraanav@gmail.com>
2021-04-15 17:59:02 -0300

    Creating project with Vue Cli and interpolating component property

---
+++ components/component-intro/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>CONTADORES</h1>
+    <h2>{{ counter }}</h2>
+    <button @click="counter++">+</button>
+    <button @click="counter--">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  }
+}
+</script>
+
+<style>
+  button {
+    margin-right: 10px;
+  }
+</style>
