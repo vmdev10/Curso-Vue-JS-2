2a38f5212493c96f685b690c29ac2d19ee0dc953
Vitória Moura <mouraanav@gmail.com>
2021-04-18 17:12:31 -0300

    Direct communication with props

---
+++ components/comunication/src/components/UsuarioInfo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Nome do usuário: <strong>{{ userName }}</strong> </p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['userName']
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
