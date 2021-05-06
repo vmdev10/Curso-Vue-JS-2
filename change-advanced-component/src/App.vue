e70bd4fd0e4eb111a1adf28180059c18dec5ad4e
vmdev10 <mouraanav@gmail.com>
2021-05-06 15:25:38 -0300

    Toggle dinamic components and pass information throught slot tag

---
+++ change-advanced-component/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+	<div id="app">
+		<span>
+			<button @click="component='Vermelho'" class="vermelho">Carregar Componente Vermelho</button>
+			<button @click="component='Verde'" class="verde">Carregar Componente Verde</button>
+			<button @click="component='Azul'" class="azul">Carregar Componente Azul</button>
+		</span>
+		<component :is="component">
+			<p>Este conteúdo vai para o {{component}}</p>
+		</component>
+	</div>
+</template>
+
+<script>
+import Vermelho from './components/Vermelho.vue'
+import Verde from './components/Verde.vue'
+import Azul from './components/Azul.vue'
+
+export default {
+	name: 'app',
+	components: { Vermelho, Verde, Azul },
+	data() {
+		return {
+			component: ''
+		}
+	}
+}
+</script>
+
+<style>
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+
+	button {
+		padding: 10px;
+		color: #FFF;
+		font-size: 1.3rem;
+		cursor: pointer;
+	}
+
+    .caixa {
+        color: #FFF;
+        font-size: 1.8rem;
+        padding: 25px;
+		margin: 10px 0px;
+    }
+
+	.vermelho {
+        border: 2px solid red;
+        background-color: #f54235;
+    }
+
+	.verde {
+        border: 2px solid green;
+        background-color: #49b057;
+    }
+
+	.azul {
+        border: 2px solid blue;
+        background-color: #2594f0;
+    }
+</style>
